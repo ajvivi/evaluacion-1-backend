e8e800a2a987737bc7440cb0254cde5be603b304
ajvivi (javier palma) <javierypd@gmail.com>
2026-09-13 18:17:39 -0300

    Se desarrolla primera_app con 2 vistas funcionales y accesibles desde el navegador

--- django01/settings.py
+++ django01/settings.py
@@ -37,10 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app1',
-    'app2',
     'primera_app',
-    'segunda_app',
 ]
 
 MIDDLEWARE = [
