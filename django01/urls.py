8ff418cfc92e2337a0a74707c8d77d29b5a928b6
ajvivi (javier palma) <javierypd@gmail.com>
2026-09-13 18:41:01 -0300

    resolviendo conflicto del merge pull rama2

--- django01/urls.py
+++ django01/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('primera/', include('primera_app.urls')),
     path('segunda/', include('segunda_app.urls')),
 ]
